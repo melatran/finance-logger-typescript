3f9c9b30b51f27fbeaa8c4edde0240240673235b
Melanie Tran <melaniexx02@gmail.com>
2020-10-30 09:58:46 -0600

    Delete tuples

--- src/app.ts
+++ src/app.ts
@@ -27,17 +27,3 @@ form.addEventListener('submit', (e: Event) => {
 
   list.render(doc, type.value, 'end');
 });
-
-// TUPLES
-let arr = ['ryu', 25, true];
-arr[0] = false;
-arr[1] = 'yoshi';
-arr = [30, false, 'yoshi'];
-
-let tup: [string, number, boolean] = ['ryu', 25, true];
-// tup[0] = false;
-tup[0] = 'ken';
-
-let student: [string, number];
-//student = [23564, 'chun-li'];
-student = ['chun-li', 23564];
